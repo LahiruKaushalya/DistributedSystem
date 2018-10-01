229f197c7ffa81a32fc079b43af434a498a8293e
Lahiru Kaushalya Ananda <lahiruk.14@cse.mrt.ac.lk>
2018-10-01 18:37:35 +0530

    Add main UI frame

---
+++ src/CS4262/MainController.java
@@ -0,0 +1,12 @@
+package CS4262;
+
+/**
+ *
+ * @author Lahiru Kaushalya
+ */
+public class MainController {
+    
+    public static void main(String args[]){
+        new MainFrame().setVisible(true);
+    }
+}
